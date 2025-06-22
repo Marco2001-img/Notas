11587cba692d91cd2548c418526bfb00b0ce77a9
marco@nebulaconsultoria.com <marco@nebulaconsultoria.com>
2025-06-22 15:05:07 -0600

    project completion v2

--- notasPG/urls.py
+++ notasPG/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/clientes/verClientes/', permanent=False)),
     path('admin/', admin.site.urls),
     path('empleados/', include('empleados.urls')),
     path('clientes/', include('clientes.urls')),
